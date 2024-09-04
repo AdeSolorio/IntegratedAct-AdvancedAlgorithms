7002050e4946acd41f9b5461ee1d86a72a34594e
AdelaSolorio <a01637205@tec.mx>
2024-09-04 09:43:44 -0600

    the manacher function is now working

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
-using namespace std;
+#include "manacher.h"
+#include <string>
+
+// to compile: g++ main.cpp manacher.cpp -o main
+// to run: ./main
+
+// falta hacer que lea los archivos
+// falta que imprima los resultados de cada archivo
 
 int main(){
-
+  std::string s = "abacdfgdcabba123321efedcba4567654abcdedcba098890fedcbaabcdefedcba0123210fabacaf";
+  manacher(s);
   return 0;
 }
---
+++ manacher.h
@@ -0,0 +1,9 @@
+#ifndef MANACHER_H
+#define MANACHER_H
+
+#include <string>
+
+void manacher(std::string &s);
+std::string turnOdd(std::string &s);
+
+#endif // MANACHER_H
