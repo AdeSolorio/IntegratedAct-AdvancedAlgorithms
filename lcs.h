5524f353a73ffa573a013e23af715909e38581b9
Andres Benjamin Antelis Moreno <bantelis10@gmail.com>
2024-09-06 15:15:27 -0600

    Implemented Longest Common Substring finder algorithm

---
+++ lcs.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <utility>
+
+std::pair<int, int> lcs(const std::string& s1, const std::string& s2);
+void lcsCaller(const std::string& t1, const std::string& t2);
